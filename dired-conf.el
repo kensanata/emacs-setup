66dedd2ba6c520fbd365352b3d346e2dca378b89
Alex Schroeder <alex@gnu.org>
2015-11-12 13:06:50 +0100

    dired: gun binaries, ls switches

--- dired-conf.el
+++ dired-conf.el
@@ -1,4 +1,8 @@
 (require 'dired-x)
+
+;; Homebrew: brew install coreutils gives us GNU ls
+(add-to-list 'exec-path "/usr/local/opt/coreutils/libexec/gnubin")
+
 (setq dired-recursive-deletes 'always
       dired-recursive-copies 'always
       dired-deletion-confirmer 'y-or-n-p
@@ -6,4 +10,5 @@
       delete-by-moving-to-trash t
       ;; trash-directory "~/.Trash/emacs"
       dired-dwim-target t
-      dired-guess-shell-alist-user '(("." "open")))
+      dired-guess-shell-alist-user '(("." "open"))
+      dired-listing-switches "-alv")
