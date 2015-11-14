65dc5239b20dea758464c6214b859749e0790635
Alex Schroeder <alex@gnu.org>
2015-11-14 16:24:47 +0100

    info-conf.el: use run-with-idle-timer

--- config/info-conf.el
+++ config/info-conf.el
@@ -1,6 +1,13 @@
-(setq Info-additional-directory-list
-      (append (list 
-               "/usr/local/opt/emacs-mac/share/info/emacs"
-               "~/src/emacs-live/packs/stable/git-pack/lib/magit/Documentation")
-              (delete "/usr/local/opt/emacs-mac/share/info"
-                      (file-expand-wildcards "/usr/local/opt/*/share/info"))))
+;; On my laptop with its external drive, this operation takes a while.
+;; That's why we're running it off an idle timer in order to improve
+;; startup time.
+
+(run-with-idle-timer
+ 10 nil
+ (lambda ()
+   (setq Info-additional-directory-list
+	 (nconc '("/usr/local/opt/emacs-mac/share/info/emacs"
+		  "~/src/emacs-live/packs/stable/git-pack/lib/magit/Documentation")
+		(delete "/usr/local/opt/emacs-mac/share/info"
+			(file-expand-wildcards "/usr/local/opt/*/share/info"))))
+   (message "Info-additional-directory-list was set")))
