b48f1655e02320fba9fca604a64202dc036e6bb0
Alex Schroeder <alex@gnu.org>
2015-12-02 23:20:40 +0100

    Using mpg321 on the Mac

--- config/emms-conf.el
+++ config/emms-conf.el
@@ -1,6 +1,6 @@
-
-;; These changes only affect my Windows setup.
-;; Sadly, I installed mpg123 instead of mpg321.
+;; Most of these changes are to get things to work on my Windows
+;; machine. Sadly, I installed mpg123 instead of mpg321 on the Windows
+;; machine.
 
 (dolist (dir '("C:/Program Files/mp3info-0.8.5-win"
 	       "C:/Program Files (x86)/mpg123-1.22.0-x86-64"))
@@ -11,12 +11,14 @@
   (when (file-directory-p dir)
     (setq emms-source-file-default-directory dir)))
 
-(setq emms-player-list '(emms-player-mpg123))
 (run-with-idle-timer
  10 nil
  (lambda ()
    (require 'emms-setup)
    (emms-standard)
-   (define-emms-simple-player mpg123 '(file url)
-     (emms-player-simple-regexp "mp3" "mp2")
-     "mpg123")))
+   (if (executable-find "mpg321")
+       (setq emms-player-list '(emms-player-mpg321))
+     (define-emms-simple-player mpg123 '(file url)
+       (emms-player-simple-regexp "mp3" "mp2")
+       "mpg123")
+     (setq emms-player-list '(emms-player-mpg123)))))
