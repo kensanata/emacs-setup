5b9864e5381a01e9bb4eb62e1656a3dfb316568d
Alex Schroeder <alex@gnu.org>
2016-02-16 13:03:33 +0100

    Use nXML for SVG files; disable auto-image-file-mode
    The problem is that auto-image-file-mode somehow messes with SVG files
    such that embedded UTF-8 encoded characters such as the unbreakable
    space are displayed as their raw bytes.

--- config/image-conf.el
+++ config/image-conf.el
@@ -1,3 +1,6 @@
+(add-to-list 'auto-mode-alist '("\\.svg\\'" . nxml-mode))
+(auto-image-file-mode 0)
+
 (defadvice image-toggle-display-text (before asc:editable-images activate)
   "Remove the intangible text property from the buffer."
   (let ((inhibit-read-only t)
