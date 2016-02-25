a81fa007833668633fc07b00a374653fe01788f9
Alex Schroeder <alex@gnu.org>
2016-02-25 16:36:58 +0100

    font-conf: get rid of Fira Code if Consolas is available
    I've had a few situations where the frame started flickering
    and Emacs was unusable on Windows. Perhaps it was due to the
    font?

--- config/font-conf.el
+++ config/font-conf.el
@@ -1,7 +1,7 @@
 
-(cond ((find-font (font-spec :name "Fira Code"))
+(cond ((find-font (font-spec :name "Consolas"))
+       (set-face-attribute 'default nil :family "Consolas" :height 130))
+      ((find-font (font-spec :name "Fira Code"))
        (set-face-attribute 'default nil :family "Fira Code" :height 160)
        (when (functionp 'mac-auto-operator-composition-mode)
-	 (mac-auto-operator-composition-mode)))
-      ((find-font (font-spec :name "Consolas"))
-       (set-face-attribute 'default nil :family "Consolas" :height 160)))
+	 (mac-auto-operator-composition-mode))))
