e9bd206c8da75df2dbceab2f499bfd7133058788
Alex Schroeder <alex@gnu.org>
2016-03-13 08:03:01 +0100

    Trying to go with a smaller font size
    Changed from :height 160 to :height 140.

--- config/font-conf.el
+++ config/font-conf.el
@@ -1,6 +1,6 @@
 
 (cond ((find-font (font-spec :name "Fira Code"))
-       (set-face-attribute 'default nil :family "Fira Code" :height 160)
+       (set-face-attribute 'default nil :family "Fira Code" :height 140)
        (when (functionp 'mac-auto-operator-composition-mode)
 	 (mac-auto-operator-composition-mode)))
       ((find-font (font-spec :name "Consolas"))
