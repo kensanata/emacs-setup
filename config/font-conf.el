ab159c2325c49d2f25c8825065fd92374971624a
Alex Schroeder <alex@gnu.org>
2016-03-13 08:08:50 +0100

    Font config: Use Consolas only on Windows

--- config/font-conf.el
+++ config/font-conf.el
@@ -1,7 +1,8 @@
 
-(cond ((find-font (font-spec :name "Fira Code"))
+(cond ((and (eq (window-system) 'w32)
+	    (find-font (font-spec :name "Consolas")))
+       (set-face-attribute 'default nil :family "Consolas" :height 130))
+      ((find-font (font-spec :name "Fira Code"))
        (set-face-attribute 'default nil :family "Fira Code" :height 140)
        (when (functionp 'mac-auto-operator-composition-mode)
-	 (mac-auto-operator-composition-mode)))
-      ((find-font (font-spec :name "Consolas"))
-       (set-face-attribute 'default nil :family "Consolas" :height 160)))
+	 (mac-auto-operator-composition-mode))))
