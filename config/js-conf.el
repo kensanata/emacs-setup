4e05bebc4bcb90fb3a355ccc69f20ed96ec4fa51
Alex Schroeder <alex@gnu.org>
2016-03-17 08:02:58 +0100

    More about Javascript comments

--- config/js-conf.el
+++ config/js-conf.el
@@ -5,9 +5,13 @@
 	    (setq indent-tabs-mode nil
 		  js-indent-level 2)
 	    (idle-highlight-mode 1)
+	    ;; https://alexschroeder.ch/wiki/2016-03-06_Javascript_Comments
 	    (setq-local comment-auto-fill-only-comments t)
 	    (setq-local comment-multi-line t)
 	    (auto-fill-mode 1)
+	    (setq c-comment-prefix-regexp "//+\\|/?\\**")
+	    (let ((c-buffer-is-cc-mode t))
+	      (c-setup-paragraph-variables))
 	    (local-set-key (kbd "RET") 'c-indent-new-comment-line)
 	    ;; (rainbow-identifiers-mode)
 	    (when (string-match "\.svg$" (buffer-file-name))
