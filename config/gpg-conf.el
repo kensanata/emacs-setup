8bd408ca0a206657cec7d8e6c749dfe95d0acea8
Alex Schroeder <alex@gnu.org>
2016-05-15 14:51:07 +0200

    gpg-conf.el based on the GGGG
    See https://github.com/kensanata/ggg#gmail-gnus-gpg-guide-gggg

---
+++ config/gpg-conf.el
@@ -0,0 +1,34 @@
+;; https://www.emacswiki.org/emacs/Gmail,_Gnus_and_GPG_on_a_Mac
+(defun gpg-restart-agent ()
+  "This kills and restarts the gpg-agent.
+
+To kill gpg-agent, we use killall. If you know that the agent is
+OK, you should just reload the environment file using
+`gpg-reload-agent-info'."
+  (interactive)
+  (shell-command "killall gpg-agent")
+  (shell-command "gpg-agent --daemon --enable-ssh-support --write-env-file")
+  ;; read the environment file instead of parsing the output
+  (gpg-reload-agent-info))
+
+(defun gpg-reload-agent-info ()
+  "Reload the ~/.gpg-agent-info file."
+  (interactive)
+  (with-temp-buffer
+    (insert-file (expand-file-name "~/.gpg-agent-info"))
+    (goto-char (point-min))
+    (while (re-search-forward "\\([A-Z_]+\\)=\\(.*\\)" nil t)
+      (setenv (match-string 1) (match-string 2)))))
+
+(defun gpg-agent-startup ()
+  "Initialize the gpg-agent if necessary.
+
+Note that sometimes the gpg-agent can be up and running and still
+be useless, in which case you should restart it using
+`gpg-restart-agent'."
+  (gpg-reload-agent-info)
+  (let ((id (string-to-number (getenv "SSH_AGENT_PID"))))
+    (unless (member id (list-system-processes))
+      (gpg-restart-agent))))
+
+(gpg-agent-startup)
