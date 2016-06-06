1f9619c5f7a376cc33d6cad21505659fa06a66b5
Alex Schroeder <alex@gnu.org>
2016-06-06 11:15:21 +0200

    Adding endless/fill-or-unfill

---
+++ config/text-config.el
@@ -0,0 +1,13 @@
+;; http://endlessparentheses.com/fill-and-unfill-paragraphs-with-a-single-key.html
+(defun endless/fill-or-unfill ()
+  "Like `fill-paragraph', but unfill if used twice."
+  (interactive)
+  (let ((fill-column
+         (if (eq last-command 'endless/fill-or-unfill)
+             (progn (setq this-command nil)
+                    (point-max))
+           fill-column)))
+    (call-interactively #'fill-paragraph)))
+
+(global-set-key [remap fill-paragraph]
+                #'endless/fill-or-unfill)
