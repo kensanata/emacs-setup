2bfa0486fb09b152bb3bf1278c98a56584985ac2
Alex Schroeder <alex@gnu.org>
2016-10-17 16:13:21 +0200

    Improve increment-number-at-point
    Also assign it to C-+.

--- config/editing-conf.el
+++ config/editing-conf.el
@@ -1,9 +1,21 @@
-(defun increment-number-at-point ()
-  (interactive)
+(defun increment-number-at-point (&optional num)
+  "Increment number at point by 1.
+Works with numerical arguments, too.
+With a negative argument (just M--), uses -1.
+With a universal argument (just C-u), ask by how much."
+  (interactive "P")
   (skip-chars-backward "0123456789")
   (or (looking-at "[0123456789]+")
       (error "No number at point"))
-  (replace-match (number-to-string (1+ (string-to-number (match-string 0))))))
+  (cond ((null num)
+	 (setq num 1))
+	((eq num '-)
+	 (setq num -1))
+	((listp num)
+	 (setq num (read-number "Increment by how much? " 1))))
+  (replace-match (number-to-string (+ num (string-to-number (match-string 0))))))
+
+(global-set-key (kbd "C-+") 'increment-number-at-point)
 
 (defun increment-numbers-in-region (start end)
   (interactive "r")
