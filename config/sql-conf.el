ab4f0a20119e83f93f64b2a81494687082427741
Alex Schroeder <alex@gnu.org>
2016-11-07 20:11:23 +0100

    Keybindings for SQL mode

--- config/sql-conf.el
+++ config/sql-conf.el
@@ -1,2 +1,25 @@
 ;; Oracle
 (setenv "NLS_LANG" "GERMAN_SWITZERLAND.UTF8")
+
+(add-hook 'sql-mode-hook 'asc:sql-init)
+
+(defun asc:sql-init ()
+  (local-set-key (kbd "C-z") 'sql-oracle)
+  (local-set-key (kbd "C-c C-d") 'asc:sql-describe-word-at-point)
+  (local-set-key (kbd "C-<return>") 'sql-send-paragraph))
+
+(add-hook 'sql-interactive-mode-hook 'asc:sql-interactive-init)
+
+(defun asc:sql-interactive-init ()
+  (local-set-key (kbd "C-z")
+		 (lambda ()
+		   (interactive)
+		   (if (> (count-windows) 1)
+		       (delete-window)
+		     (bury-buffer)))))
+
+(defun asc:sql-describe-word-at-point ()
+  (interactive)
+  (let ((word (symbol-name (symbol-at-point))))
+    (when word
+      (sql-send-string (concat "describe " word)))))
