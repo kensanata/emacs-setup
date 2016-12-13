ee1c726b2b3ff8bc4aab1a05ccd514951a17135b
Alex Schroeder <alex@gnu.org>
2016-12-13 20:41:49 +0100

    Install cider package if necessary

--- config/cider-conf.el
+++ config/cider-conf.el
@@ -1,2 +1,6 @@
+(unless (package-installed-p 'cider)
+  (package-install 'cider))
+
 (when (file-directory-p (concat (getenv "HOME") "/.lein"))
   (setenv "LEIN_HOME" (concat (getenv "HOME") "/.lein")))
+
