575e804d3e395b4e039624bc71a484a291e74c58
Alex Schroeder <alex@gnu.org>
2017-01-23 21:59:07 +0100

    Improved increment-number

--- config/editing-conf.el
+++ config/editing-conf.el
@@ -5,9 +5,9 @@ With a negative argument (just M--), uses -1.
 With a universal argument (just C-u), ask by how much."
   (interactive "P")
   (save-excursion
-    (when (zerop (skip-chars-backward "0123456789"))
+    (when (zerop (skip-chars-backward "-0123456789."))
       (skip-syntax-forward "-"))
-    (or (looking-at "[0123456789]+")
+    (or (looking-at "-?[0123456789.]+")
 	(error "No number at point"))
     (cond ((null num)
 	   (setq num 1))
@@ -22,7 +22,7 @@ With a universal argument (just C-u), ask by how much."
 (defun increment-numbers-in-region (start end)
   (interactive "r")
   (goto-char start)
-  (while (re-search-forward "[0123456789]+" end t)
+  (while (re-search-forward "" end t)
     (replace-match (number-to-string (1+ (string-to-number (match-string 0)))))))
 
 ;; Add FIXME highlighting everywhere
