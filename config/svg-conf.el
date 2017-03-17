1c08792e0be3817933b60523c26f0e890638ab5f
Alex Schroeder <alex@gnu.org>
2017-03-17 08:31:40 +0100

    svg: added convenience function for Gridmapper

--- config/svg-conf.el
+++ config/svg-conf.el
@@ -1,6 +1,6 @@
 ;; Helper functions for editing SVG files
 
-(defun rotate-coordinates (start end)
+(defun as:rotate-coordinates (start end)
   (interactive "r")
   (goto-char start)
   (while (re-search-forward "\\([0-9]+\\),\\([0-9]+\\)" end t)
@@ -9,4 +9,12 @@
 	   (x2 y)
 	   (y2 (- 10 x)))
       (replace-match (format "%d,%d" x2 y2)))))
+
+(defun as:increase-step-numbers (start end)
+  (interactive "r")
+  (goto-char start)
+  (save-excursion
+    (while (re-search-forward "step\\([0-9]+\\)" end t)
+      (let ((n (string-to-number (match-string 1))))
+	(replace-match (format "step%d" (1+ n)))))))
     
