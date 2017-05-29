3d46ed5added71a660cfd048e0dcb8f8069246d0
Alex Schroeder <alex@gnu.org>
2017-05-29 10:10:07 +0200

    Added plink

--- config/tramp-conf.el
+++ config/tramp-conf.el
@@ -8,10 +8,18 @@
 	 (tramp-parse-rhosts "/etc/hosts.equiv")
 	 (tramp-parse-shosts "~/.ssh/known_hosts"))))
 
+(defvar putty-directory "c:/Program Files/PuTTY"
+  "The directory containing pscp.exe and plink.exe on Windows.")
+
 (defun sibirocobombus ()
   (interactive)
   "Connect to my favorite server using Tramp."
-  (find-file "/ssh:alex@alexschroeder.ch#882:"))
+  (if (not (eq window-system 'w32))
+      (find-file "/ssh:alex@alexschroeder.ch#882:")
+    (when (and (not (string-match putty-directory (getenv "PATH")))
+	       (file-directory-p putty-directory))
+      (setenv "PATH" (concat putty-directory ";" (getenv "PATH"))))
+    (find-file "/plink:alex@alexschroeder.ch#882:")))
 
 (defun the-shire ()
   (interactive)
