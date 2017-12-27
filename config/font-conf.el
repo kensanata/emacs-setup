ccf2300353256f25d26744b195f424ce3fb9b73c
Alex Schroeder <alex@localhost.localdomain>
2017-12-27 20:51:19 +0100

    Prefer Noto Mono over Fira Code

--- config/font-conf.el
+++ config/font-conf.el
@@ -2,6 +2,8 @@
 (cond ((and (eq (window-system) 'w32)
 	    (find-font (font-spec :name "Consolas")))
        (set-face-attribute 'default nil :family "Consolas" :height 130))
+      ((find-font (font-spec :name "Noto Mono"))
+       (set-face-attribute 'default nil :family "Noto Mono" :height 140))
       ((find-font (font-spec :name "Fira Code"))
        (set-face-attribute 'default nil :family "Fira Code" :height 140)
        (when (functionp 'mac-auto-operator-composition-mode)
