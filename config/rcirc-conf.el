b3979fb26b4384f7afc8f037259f91cc87030521
Alex Schroeder <alex@gnu.org>
2018-02-11 08:52:18 +0100

    rcirc-colors: delay setting of colors
    We must wait until the default background color is known.

--- config/rcirc-conf.el
+++ config/rcirc-conf.el
@@ -114,23 +114,24 @@
 
 ;; To check out the list, evaluate (list-colors-display rcirc-colors)
 ;; To reset map (setq rcirc-color-mapping (make-hash-table :test 'equal))
-(setq rcirc-colors
-      (let ((candidates nil)
-	    (color nil)
-	    (y1 (rcirc-colors-Y (face-background 'default))))
-        (dolist (item color-name-rgb-alist)
-	  (setq color (car item))
-	  (unless (color-gray-p color)
-	    ;; Contrast ration is (Y(b) + 0.05) / (Y(d) + 0.05) where
-	    ;; Y(b) is the brightness (luminance) of the brighter
-	    ;; color and Y(d) is the brightness of the darker color.
-	    (let* ((y2 (rcirc-colors-Y color))
-		   (r (if (> y1 y2)
-			  (/ (+ y1 0.05) (+ y2 0.05))
-			(/ (+ y2 0.05) (+ y1 0.05)))))
-	      (when (> r 4.5)
-		(setq candidates (cons color candidates))))))
-	  candidates))
+(eval-after-load 'rcirc
+  '(setq rcirc-colors
+	 (let ((candidates nil)
+	       (color nil)
+	       (y1 (rcirc-colors-Y (face-background 'default))))
+	   (dolist (item color-name-rgb-alist)
+	     (setq color (car item))
+	     (unless (color-gray-p color)
+	       ;; Contrast ration is (Y(b) + 0.05) / (Y(d) + 0.05) where
+	       ;; Y(b) is the brightness (luminance) of the brighter
+	       ;; color and Y(d) is the brightness of the darker color.
+	       (let* ((y2 (rcirc-colors-Y color))
+		      (r (if (> y1 y2)
+			     (/ (+ y1 0.05) (+ y2 0.05))
+			   (/ (+ y2 0.05) (+ y1 0.05)))))
+		 (when (> r 4.5)
+		   (setq candidates (cons color candidates))))))
+	   candidates)))
 
 (eval-after-load 'rcirc '(require 'rcirc-color))
 (eval-after-load 'rcirc '(require 'rcirc-menu))
