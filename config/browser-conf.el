9b5f3500b2c1551ac0df9592cba9ed93699cca7e
Alex Schroeder <alex@gnu.org>
2018-02-18 18:36:23 +0100

    browser: add browse-url-feh command for images

--- config/browser-conf.el
+++ config/browser-conf.el
@@ -1,2 +1,7 @@
 ;; I want to use eww.
 (setq browse-url-browser-function 'eww-browse-url)
+
+(defun browse-url-feh (url &rest args)
+  "Browse URL at point using feh(1)."
+  (interactive (browse-url-interactive-arg "Image URL: "))
+  (async-shell-command (concat "feh " url)))
