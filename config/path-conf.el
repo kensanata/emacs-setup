a95e799fe1e35c2d1913bed7b213c62717740c2b
Alex Schroeder <alex@gnu.org>
2018-02-19 17:16:48 +0100

    path: make it work on Windows

--- config/path-conf.el
+++ config/path-conf.el
@@ -1,8 +1,8 @@
 ;; local python binaries
 (defun asc:add-to-PATH (path)
   (setq path (expand-file-name path))
-  (let ((paths (split-string (getenv "PATH") ":")))
+  (let ((paths (parse-colon-path (getenv "PATH"))))
     (unless (member path paths)
-      (setenv "PATH" (mapconcat 'identity (cons path paths) ":")))))
+      (setenv "PATH" (mapconcat 'identity (cons path paths) path-separator)))))
 
 (asc:add-to-PATH "~/.local/bin")
