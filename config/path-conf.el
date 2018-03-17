1aefb2fdd0c7218bca413a2bb096e8f89ce8bdf2
Alex Schroeder <alex@gnu.org>
2018-03-17 01:14:37 +0100

    path: don't add duplicates

--- config/path-conf.el
+++ config/path-conf.el
@@ -1,8 +1,11 @@
 ;; local python binaries
+(defun asc:add-to-env (var path)
+  (let ((paths (parse-colon-path (getenv var))))
+    (unless (member path paths)
+      (setenv var (mapconcat 'identity (cons path paths) path-separator)))))
+
 (defun asc:add-to-PATH (path)
   (setq path (expand-file-name path))
-  (let ((paths (parse-colon-path (getenv "PATH"))))
-    (unless (member path paths)
-      (setenv "PATH" (mapconcat 'identity (cons path paths) path-separator)))))
+  (asc:add-to-env "PATH" path))
 
-(asc:add-to-PATH "~/.local/bin")
+(asc:add-to-PATH "~/.local/bin/")
