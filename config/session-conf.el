05f3e2d837986788fe07cce6c488b0ed39f3285b
Alex Schroeder <alex@gnu.org>
2018-04-17 21:10:12 +0900

    No longer save kill ring in session globals
    For one, it's easy to save passwords this way, and when the kill ring
    contains formatted text, the text properties aren't stripped from the
    kill ring and thus the session file will cause a read error when it
    gets read the next time Emacs starts up again, defeating the purpose
    of the entire thing.

--- config/session-conf.el
+++ config/session-conf.el
@@ -4,3 +4,5 @@
 
 ;; prevent point from being wrong in commit messages by magit
 (setq session-name-disable-regexp "\\.git/[A-Z_]+\\'")
+;; prevent parse errors for fontified text in the kill-ring
+(add-to-list 'session-globals-exclude 'kill-ring)
