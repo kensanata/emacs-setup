b6c1f814421074bdc5063896c699f4607cb82e7f
Alex Schroeder <alex@gnu.org>
2018-04-17 21:11:40 +0900

    hex-describe: fiddling with the mode

--- config/hex-describe-conf.el
+++ config/hex-describe-conf.el
@@ -5,8 +5,10 @@
     ("\\*.*?\\*" . 'bold)
     ("\\[\\([1-9][0-9]*d[1-9][0-9]*\\(?:x[1-9][0-9]*\\)?\\(?:\\+[1-9][0-9]*\\)?\\)\\]"
      (0 font-lock-keyword-face) (1 font-lock-string-face t))
-    "\\[.*?\\]"
+    ("\\[[^][\n]*\\(\\[[^][\n]*\\]\\)[^][\n]*\\]"
+     (0 font-lock-keyword-face) (1 font-lock-constant-face t))
+    "\\[[^][\n]*\\]"
     ("^[0-9]+," . font-lock-type-face))
-  '("hex-describe-default-.*\\.txt$")
+  '("hex-describe-.*\\.txt$")
   nil
   "A major mode to edit Hex Describe data files.")
