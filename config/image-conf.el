05bafbc20b7144981e9cc16f9141919b6437bd1e
Alex Schroeder <alex@gnu.org>
2018-07-18 10:47:20 +0200

    image: added some keybindings
    Added p, n and d for image-dired-display-image-mode.

--- config/image-conf.el
+++ config/image-conf.el
@@ -9,3 +9,32 @@
     (remove-list-of-text-properties (point-min) (point-max)
 				    '(intangible))
     (set-buffer-modified-p modified)))
+
+(add-hook 'image-dired-display-image-mode-hook 'asc:image-dired-display)
+
+(defun asc:image-dired-display ()
+  "Some more initialisation for `image-dired-display-image-mode'."
+  (local-set-key (kbd "p") 'asc:image-dired-display-previous)
+  (local-set-key (kbd "n") 'asc:image-dired-display-next)
+  (local-set-key (kbd "d") 'asc:image-dired-display-delete))
+
+(defun asc:image-dired-display-previous ()
+  "Show previous image based on the *image-dired* buffer."
+  (interactive)
+  (with-current-buffer (get-buffer "*image-dired*")
+    (image-dired-backward-image)
+    (image-dired-display-thumbnail-original-image)))
+
+(defun asc:image-dired-display-next ()
+  "Show next image based on the *image-dired* buffer."
+  (interactive)
+  (with-current-buffer (get-buffer "*image-dired*")
+    (image-dired-forward-image)
+    (image-dired-display-thumbnail-original-image)))
+
+(defun asc:image-dired-display-delete ()
+  "Mark image as deleted in the *image-dired* buffer."
+  (interactive)
+  (with-current-buffer (get-buffer "*image-dired*")
+    (image-dired-flag-thumb-original-file)
+    (image-dired-display-thumbnail-original-image)))
