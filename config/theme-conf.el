ad23ecc7e840dc11447a9b09fc31f000c2e73020
Alex Schroeder <alex@gnu.org>
2018-07-31 08:28:59 +0200

    theme: using brutalist-theme

--- config/theme-conf.el
+++ config/theme-conf.el
@@ -1,7 +1,13 @@
 (setq custom-theme-directory "~/.emacs.d/emacs-setup/lib")
+
 ;; Use M-x enable-theme and M-x disable-theme
 (condition-case err
     (load-theme 'pink-bliss t t)
+  (error))
+
+(add-to-list 'custom-theme-load-path "/home/alex/src/brutalist-theme.el")
+(condition-case err
+    (load-theme 'brutalist t t)
   (error))
 
 (asc:package-install 'foggy-night-theme)
