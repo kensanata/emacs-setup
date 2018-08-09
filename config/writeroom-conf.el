1cb29e56d3397d94e65ba65697637d1d10607859
Alex Schroeder <alex@gnu.org>
2018-08-09 17:19:58 +0200

    Add agressive writeroom-mode

---
+++ config/writeroom-conf.el
@@ -0,0 +1,6 @@
+(asc:package-install 'writeroom-mode)
+
+(add-hook 'text-mode-hook 'writeroom-mode)
+(add-hook 'prog-mode-hook 'writeroom-mode)
+(add-hook 'rcirc-mode-hook 'writeroom-mode)
+(add-hook 'rcirc-menu-mode-hook 'writeroom-mode)
