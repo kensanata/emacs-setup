bc14ed62516de5dae7cbf5627f6689f339ef72bc
Alex Schroeder <alex@gnu.org>
2018-10-01 09:42:55 +0200

    font: switching to Iosevka

--- config/font-conf.el
+++ config/font-conf.el
@@ -1,9 +1,8 @@
 ;; See https://github.com/tonsky/FiraCode/wiki/Setting-up-Emacs
-(cond ((and (eq (window-system) 'w32)
-	    (find-font (font-spec :name "Consolas")))
-       (set-face-attribute 'default nil :family "Consolas" :height 130))
-      ((find-font (font-spec :name "Iosevka"))
-       (set-face-attribute 'default nil :family "Iosevka" :height 220))
+(cond ((find-font (font-spec :name "Iosevka"))
+       (set-face-attribute 'default nil :family "Iosevka" :height 220)
+       ;; on windows, this defaults to a different font?
+       (set-face-attribute 'fixed-pitch nil :family "Iosevka" :weight 'light :height 220))
       ((find-font (font-spec :name "Noto Mono"))
        (set-face-attribute 'default nil :family "Noto Mono" :height 200)))
 
