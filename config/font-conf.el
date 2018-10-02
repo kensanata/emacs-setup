5afb633368f0cfb8295d6177a8bf40c123a91868
Alex Schroeder <alex@gnu.org>
2018-10-02 08:50:50 +0200

    font and rcirc: W32 settings reintroduced

--- config/font-conf.el
+++ config/font-conf.el
@@ -1,8 +1,11 @@
 ;; See https://github.com/tonsky/FiraCode/wiki/Setting-up-Emacs
 (cond ((find-font (font-spec :name "Iosevka"))
-       (set-face-attribute 'default nil :family "Iosevka" :height 220)
        ;; on windows, this defaults to a different font?
-       (set-face-attribute 'fixed-pitch nil :family "Iosevka" :weight 'light :height 220))
+       (dolist (face '(default fixed-pitch))
+	 (set-face-attribute
+	  face nil
+	  :family "Iosevka"
+	  :height (if (eq (window-system) 'w32) 180 220))))
       ((find-font (font-spec :name "Noto Mono"))
        (set-face-attribute 'default nil :family "Noto Mono" :height 200)))
 
