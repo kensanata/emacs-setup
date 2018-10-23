c74b0f3e3069063d8545ec554acfff03f0375175
Alex Schroeder <alex@gnu.org>
2018-10-23 22:46:07 +0200

    nxml: whitespace, highlight, abbrevs, skeleton

--- config/nxml-conf.el
+++ config/nxml-conf.el
@@ -1,2 +1,18 @@
 (setq nxml-child-indent 0)
 (add-to-list 'auto-mode-alist '("\\.opml\\'" . nxml-mode))
+
+(add-hook 'nxml-mode-hook 'asc:init-nxml-mode)
+
+(defun asc:init-nxml-mode ()
+  (idle-highlight-mode 1)
+  (local-set-key (kbd "C-<tab>") 'company-complete)
+  (add-hook 'before-save-hook 'delete-trailing-whitespace t t)
+  (define-abbrev nxml-mode-abbrev-table "ol"
+    "" 'opml-outline)
+  (setq indent-tab-mode nil)
+  (setq c-basic-offset 2))
+
+(define-skeleton opml-outline
+  "Add a new entry to an OPML outline."
+  "Feed: "
+  "<outline title=\"" _ "\" xmlUrl=\"" str "\"/>\n")
