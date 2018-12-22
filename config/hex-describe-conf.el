747cc70d38de8c2c24dd433053097b1b6fb23c4d
Alex Schroeder <alex@gnu.org>
2018-12-22 13:48:25 +0100

    hex-describe: changes to highlighting

--- config/hex-describe-conf.el
+++ config/hex-describe-conf.el
@@ -1,14 +1,15 @@
 (define-generic-mode hex-describe-mode
   '("#")
   nil
-  '(("^;.*" . font-lock-function-name-face)
-    ("\\*.*?\\*" . 'bold)
+  '(("^;\\(.*\\)" (1 'underline))
+    ("\\*\\*.*?\\*\\*" . 'bold)
+    ("\\*.*?\\*" . 'italic)
     ("\\[\\([1-9][0-9]*d[1-9][0-9]*\\(?:x[1-9][0-9]*\\)?\\(?:\\+[1-9][0-9]*\\)?\\)\\]"
-     (0 font-lock-keyword-face) (1 font-lock-string-face t))
+     (0 'bold) (1 'underline t))
     ("\\[[^][\n]*\\(\\[[^][\n]*\\]\\)[^][\n]*\\]"
-     (0 font-lock-keyword-face) (1 font-lock-constant-face t))
+     (0 'bold) (1 'underline t))
     "\\[[^][\n]*\\]"
-    ("^[0-9]+," . font-lock-type-face))
+    ("^[0-9]+," . 'bold))
   '("hex-describe-.*\\.txt$")
   nil
   "A major mode to edit Hex Describe data files.")
