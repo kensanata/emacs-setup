611383bac9162852b8595c53338218a7b9cd1b05
Alex Schroeder <alex@gnu.org>
2019-01-20 10:02:29 +0100

    hex-describe: change highlighting, add minor modes
    - flyspell
    - idle-highlight

--- config/hex-describe-conf.el
+++ config/hex-describe-conf.el
@@ -2,14 +2,17 @@
   '("#")
   nil
   '(("^;\\(.*\\)" (1 'underline))
-    ("\\*\\*.*?\\*\\*" . 'bold)
-    ("\\*.*?\\*" . 'italic)
     ("\\[\\([1-9][0-9]*d[1-9][0-9]*\\(?:x[1-9][0-9]*\\)?\\(?:\\+[1-9][0-9]*\\)?\\)\\]"
      (0 'bold) (1 'underline t))
     ("\\[[^][\n]*\\(\\[[^][\n]*\\]\\)[^][\n]*\\]"
      (0 'bold) (1 'underline t))
     "\\[[^][\n]*\\]"
-    ("^[0-9]+," . 'bold))
+    ("^[0-9]+," . 'bold)
+    ("\\*\\*.*?\\*\\*" . 'bold)
+    ("\\*.*?\\*" . 'italic))
   '("hex-describe-.*\\.txt$")
   nil
   "A major mode to edit Hex Describe data files.")
+
+(add-hook 'hex-describe-mode-hook 'turn-on-flyspell)
+(add-hook 'hex-describe-mode-hook 'idle-highlight-mode)
