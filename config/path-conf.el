71cbc69983ef9d6d69d5b05e6722f7b7c9675359
Alex Schroeder <alex@gnu.org>
2019-04-11 22:24:19 +0200

    path: fixed MANPATH setup
    The problem was that the man pages for the Perl modules I had
    installed via Perlbrew weren't showing up. The problem was that
    MANPATH wasn't set correctly. Once fixed, the next problem was that
    completion didn't show the newly accessible man pages. Here, the
    problem was that man queries the index, which is created by mandb,
    which runs without the MANPATH setting, and thus ignores the man pages
    for the Perl modules I had installed. I added a defun to run mandb for
    me, in order to remind me of the problem.

--- config/path-conf.el
+++ config/path-conf.el
@@ -15,13 +15,20 @@
 (asc:add-to-PATH "~/.local/bin")
 
 ;; local man path: 
-(setenv "MANPATH" ":/home/alex/perl5/man")
+(setenv "MANPATH" "/home/alex/perl5/perlbrew/perls/perl-5.28.1/man:/home/alex/perl5/man:/usr/local/man:/usr/local/share/man:/usr/share/man")
+
+(defun mandb ()
+  "Update the man database after installing new packgages for Perl.
+The important part is that we run mandb for the current user, using the
+current MANPATH environment variable."
+  (interactive)
+  (shell-command "mandb"))
 
 ;; Perl 5
 ;; This is wrong: on sibirocobombus this bin is full of scripts wanting 5.24
-;; (asc:add-to-PATH "~/perl5/bin/")
+(asc:add-to-PATH "~/perl5/bin/")
 ;; This is wrong: "ListUtil.c: loadable library and perl binaries are mismatched (got handshake key 0xd600000, needed 0xd880000)"
-;; (setenv "PERL5LIB" "/home/alex/perl5/lib/perl5")
+(setenv "PERL5LIB" "/home/alex/perl5/lib/perl5")
 
 ;; Perl 6
 (asc:add-to-PATH "~/rakudo/bin/")
