107d808dac6057910d3b0231729a8ba1f42a97a8
Alex Schroeder <alex@gnu.org>
2019-04-11 22:27:37 +0200

    gplus: renamed a function

--- config/gplus-conf.el
+++ config/gplus-conf.el
@@ -3,4 +3,10 @@
 (autoload 'gplus-browse-archive "gplus-browser.el"
   "Browse G+ Archive" t)
 
-(global-set-key (kbd "C-c a") 'gplus-browse-archive)
+(global-set-key (kbd "C-c a") 'gplus-archive)
+
+
+(defun gplus-archive ()
+  "Browse my archive."
+  (interactive)
+  (gplus-browse-archive "~/Downloads/Takeout/Google+ Stream/Posts"))
