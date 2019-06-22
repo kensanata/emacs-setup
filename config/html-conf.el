ab2867708bc78112897b33851e2a425f940d46fe
Alex Schroeder <alex@gnu.org>
2019-06-22 15:41:44 +0200

    html: enable tagedit

--- config/html-conf.el
+++ config/html-conf.el
@@ -6,3 +6,11 @@
   (add-hook 'before-save-hook 'delete-trailing-whitespace t t)
   (setq c-basic-offset 2
 	tab-width 2))
+
+(eval-after-load 'sgml-mode
+  '(progn
+     (require 'tagedit)
+     (tagedit-add-paredit-like-keybindings)
+     (add-hook 'html-mode-hook (lambda ()
+				 (tagedit-mode 1)
+				 (tagedit-add-experimental-features)))))
