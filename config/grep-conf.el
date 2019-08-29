1fdaae4a26d023b0853ac5acd46c30b87ab778f0
Alex Schroeder <alex@gnu.org>
2019-08-29 15:17:08 +0200

    grep: bind C-c s to grep-word-at-point

--- config/grep-conf.el
+++ config/grep-conf.el
@@ -1,3 +1,18 @@
 ;; default to case insensitive
 (eval-after-load "grep"
   '(grep-apply-setting 'grep-command "grep --color -niH -e "))
+
+(defun grep-word-at-point (word)
+  "Grep for thing at point.
+Per default, the same extension is used as the file-name of the
+current buffer. Use a prefix argument to override either."
+  (interactive (list (thing-at-point 'word)))
+  (let* ((file-name (buffer-file-name))
+	 (extension (and file-name (file-name-extension file-name)))
+	 (pattern (if extension (concat "*." extension) "*")))
+    (when current-prefix-arg
+      (setq word (read-string "Grep for: " word)
+	    pattern (read-string "Files to grep: " pattern)))
+    (grep (format "grep --color -niH -e \"%s\" %s" word pattern))))
+
+(global-set-key (kbd "C-c s") 'grep-word-at-point)
