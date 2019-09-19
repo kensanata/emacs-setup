85ba8f5942e5dfe81d0e25eb36e5c32cd8657f4d
Alex Schroeder <alex@gnu.org>
2019-09-19 17:53:15 +0200

    dired: final version of my image editing variant
    It creates a virtual dired buffer based on a call to exiftool.

--- config/dired-conf.el
+++ config/dired-conf.el
@@ -54,35 +54,62 @@ This is used for `image-dired'."
 
 (setq image-dired-thumb-margin 5)
 
-(require 'dired-x)
+(defun asc:image-dired-exif-data (dir)
+  "Return alist of filenames and Date Time Original.
+Do this for all files in DIR, possibly remote."
+  (message "Reading exifdata for %s..." dir)
+  (let* ((default-directory dir)
+	 (data (shell-command-to-string
+		"exiftool -p '${dateTimeOriginal#;DateFmt(\"%Y-%m-%d %H:%M:%S\")} $fileName' -q -f .")))
+    (message "Reading exifdata for %s...done" dir)
+    (mapcar (lambda (line)
+	      (if (string= (substring line 0 2) "- ")
+		  (let ((name (substring line 2)))
+		    (cons name		; filename
+			  (format-time-string
+			   "%F %R" (file-attribute-modification-time
+				    (file-attributes name)))))
+		(cons
+		 (substring line 20)	  ; filename
+		 (substring line 0 19)))) ; date and time
+	    (split-string data "\n" t))))
 
 (defun asc:image-dired-exif-mode (dir)
-  "Create a virtual buffer for images sorted by creation date."
+  "Create a virtual buffer for images sorted by creation date.
+Use the apostrophe to edit the text files matching the image file."
   (interactive "DDirectory: ")
   (with-current-buffer (get-buffer-create "*images*")
-    (let ((inhibit-read-only t))
+    (let ((inhibit-read-only t)
+	  (data (asc:image-dired-exif-data dir)))
       (erase-buffer)
-      (let* ((files (directory-files dir 'full "jpg$"))
+      (let* ((files (sort (directory-files dir nil "jpg$" t)
+			  (lambda (f1 f2)
+			    (string< (cdr (assoc f1 data))
+				     (cdr (assoc f2 data))))))
 	     (prog (make-progress-reporter "Reading files " 0 (length files)))
 	     (i 0))
 	(insert dir ":\n")
 	(dolist (file files)
-	  (let* ((attr (file-attributes file 'string))
-		 (default-directory dir)	; required for eshell remote command
-	     	 (created (eshell-command-to-value
-			   (eshell-remote-command
-			    "exiftool" (list "-p" "${dateTimeOriginal#;DateFmt('%Y-%m-%d %H:%M:%S')}"
-					     "-q" "-f" (file-name-nondirectory file))))))
+	  (let ((attr (file-attributes (expand-file-name file dir) 'string)))
 	    (insert (format "%s % 3d %s %s % 10d %s %s\n"
 			    (file-attribute-modes attr)
 			    (file-attribute-link-number attr)
 			    (file-attribute-user-id attr)
 			    (file-attribute-group-id attr)
 			    (file-attribute-size attr)
-			    (substring created 0 -1) ; drop trailing newline
+			    (cdr (assoc file data))
 			    ;; (format-time-string "%F %R" (file-attribute-modification-time attr))
-			    (file-name-nondirectory file))))
+			    file)))
 	  (progress-reporter-update prog (setq i (1+ i))))
 	(progress-reporter-done prog))
-      (dired-virtual-mode))
+      (dired-virtual-mode)
+      (local-set-key (kbd "'") 'asc:label-edit))
     (switch-to-buffer (current-buffer))))
+
+(defun asc:label-edit ()
+  "Edit the text file for the image at point.
+This is used for `asc:image-dired-exif-mode'."
+  (interactive)
+  (find-file (concat (file-name-sans-extension
+		      (dired-file-name-at-point))
+		     ".txt")))
