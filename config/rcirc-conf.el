a6ec2dfefe5a787eaceda0fe9e93c64adcf78148
Alex Schroeder <alex@gnu.org>
2019-11-29 16:06:27 +0100

    Removed #gopherproject

--- config/rcirc-conf.el
+++ config/rcirc-conf.el
@@ -81,8 +81,7 @@ We have to wait for a second before joining them."
 		    "#bussard"
 		    "#fennel"
 		    "#mastodon"
-		    "#pleroma"
-		    "#gopherproject"))
+		    "#pleroma"))
 	;; SDF gopher moved to tilde.chat
 	;; ("irc.sdf.org" :channels ("#gopher"))
 	("irc.tilde.chat"
