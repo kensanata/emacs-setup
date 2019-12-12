55a5199d5d390b7fe124e11e8e120a3b5ee10f7b
Alex Schroeder <alex@gnu.org>
2019-12-12 22:41:16 +0100

    rcirc: added #jsoftware

--- config/rcirc-conf.el
+++ config/rcirc-conf.el
@@ -81,7 +81,8 @@ We have to wait for a second before joining them."
 		    "#bussard"
 		    "#fennel"
 		    "#mastodon"
-		    "#pleroma"))
+		    "#pleroma"
+		    "#jsoftware"))
 	;; SDF gopher moved to tilde.chat
 	;; ("irc.sdf.org" :channels ("#gopher"))
 	("irc.tilde.chat"
