bf302508d93ffbf0beac6b69bdb45264aa086323
Alex Schroeder <alex@gnu.org>
2019-12-30 23:40:48 +0100

    authinfo: added qr-code-region

--- config/authinfo-conf.el
+++ config/authinfo-conf.el
@@ -1,1 +1,14 @@
 (setq auth-sources '("~/.authinfo.gpg"))
+
+(defun qr-code-region (start end)
+  "Show a QR code of the region.
+A simple way to transfer text to the phone..."
+  (interactive "r")
+  (let ((buf (get-buffer-create "*QR*"))
+	(inhibit-read-only t))
+    (with-current-buffer buf
+      (erase-buffer))
+    (let ((coding-system-for-read 'raw-text))
+      (shell-command-on-region start end "qrencode -o -" buf))
+    (switch-to-buffer buf)
+    (image-mode)))
