7861bd7226c657ff8dec0596432e758c401bfddd
Alex Schroeder <Alex.Schroeder@bsi-software.com>
2020-01-20 15:52:23 +0100

    sql: switch to PostgreSQL

--- config/sql-conf.el
+++ config/sql-conf.el
@@ -1,17 +1,12 @@
-;; Oracle
-(setenv "NLS_LANG" "GERMAN_SWITZERLAND.UTF8")
-
-(setq sql-database "dev"
-      sql-user "devasc"
-      sql-password "devascpw"
-      sql-product 'oracle)
+(setq sql-database nil
+      sql-user "postgres"
+      sql-password "manager"
+      sql-product 'postgres)
 
 (add-hook 'sql-mode-hook 'asc:sql-init)
 
 (defun asc:sql-init ()
-  (local-set-key (kbd "C-z") 'sql-oracle)
-  (local-set-key (kbd "C-c d") 'asc:sql-describe-word-at-point)
-  (local-set-key (kbd "C-c c") 'asc:sql-code-word-at-point)
+  (local-set-key (kbd "C-z") 'sql-postgres)
   (local-set-key (kbd "C-<return>") 'sql-send-paragraph))
 
 (add-hook 'sql-interactive-mode-hook 'asc:sql-interactive-init)
@@ -23,15 +18,3 @@
 		   (if (> (count-windows) 1)
 		       (delete-window)
 		     (bury-buffer)))))
-
-(defun asc:sql-describe-word-at-point ()
-  (interactive)
-  (let ((word (symbol-name (symbol-at-point))))
-    (when word
-      (sql-send-string (concat "describe " word)))))
-
-(defun asc:sql-code-word-at-point ()
-  (interactive)
-  (let ((num (number-at-point)))
-    (when num
-      (sql-send-string (format "select ors_util.get_user_code(%d) from dual;" num)))))
