8775ec67503406b7913adc95fd8710fcd5070be8
Alex Schroeder <Alex.Schroeder@bsi-software.com>
2020-02-03 08:11:00 +0100

    Autoload authinfo-copy-password

--- config/authinfo-conf.el
+++ config/authinfo-conf.el
@@ -1,5 +1,7 @@
 (setq auth-sources '("~/.authinfo.gpg"))
 
+(autoload 'authinfo-copy-password "authinfo" "Copy password." t)
+
 (defun qr-code-region (start end)
   "Show a QR code of the region.
 A simple way to transfer text to the phone..."
