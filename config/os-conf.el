0ab027ee0c2259113519f56b22b78f88d1820d3a
Alex Schroeder <Alex.Schroeder@bsi-software.com>
2020-05-26 11:01:19 +0200

    os: set grep-find-command for Windows/PortableGit

--- config/os-conf.el
+++ config/os-conf.el
@@ -1,7 +1,6 @@
 (setenv "LANG" "en_GB.UTF-8")
 
 (when (eq (window-system) 'w32)
-  
   ;; (add-to-list 'dynamic-library-alist '(png "libpng15-15.dll"))
   ;; make sure cygwin comes first, for ssh
   (let ((path "C:/cygwin64/bin"))
@@ -13,19 +12,10 @@
   (setq default-file-name-coding-system 'cp1252)
   (setenv "CVS_RSH" "ssh")
   (setenv "WikiDataDir" "test-data")
+  (eval-after-load "grep"
+    '(grep-apply-setting 'grep-find-command
+			 "c:/Portable\\ Programs/PortableGit/usr/bin/find.exe . -type f -exec grep.exe --color -niH -e  \"{}\" \";\""))
   (setq visible-bell t)
   (setq mouse-drag-copy-region t)
   (setq history-length 1000)
-  ;; Tramp
-  ;; Download http://www.chiark.greenend.org.uk/~sgtatham/putty/download.html
-  ;; Docs http://the.earth.li/~sgtatham/putty/0.60/htmldoc/Chapter7.html#plink
-  (setq tramp-auto-save-directory (getenv "TEMP"))
-  ;; Printing
-  (setq ps-printer-name "\\\\fileserver06\\Zuerich_PDF-Datei"))
-
-(defun atreus-layout ()
-  "If using the Atreus on a Mac, we want the Alt key to be Meta."
-  (interactive)
-  (setq mac-option-modifier 'meta)
-  (setq default-input-method 'german-prefix)
-  (set-input-method 'german-prefix))
+  (setq tramp-auto-save-directory (getenv "TEMP")))
