5c073a569f27b1aaa640c783d5a66c685b5a86a6
Alex Schroeder <alex@gnu.org>
2020-06-29 11:46:03 +0200

    eww: handle gopher and gemini links

--- config/eww-conf.el
+++ config/eww-conf.el
@@ -2,3 +2,13 @@
 ;; use M-x url-cookie-list to check
 (setq url-cookie-trusted-urls '()
       url-cookie-untrusted-urls '(".*"))
+
+(advice-add 'eww-browse-url :around 'asc:eww-browse-url)
+
+(defun asc:eww-browse-url (original url &optional new-window)
+  "Handle gemini links."
+  (cond ((string-match-p "\\`\\(gemini\\|gopher\\)://" url)
+	 (require 'elpher)
+	 (elpher-go url))
+	(t (funcall original url new-window))))
+
