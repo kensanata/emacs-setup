383a15be4f9fe9e1de6e8c8b4e45a75f4f91780d
Alex Schroeder <alex@gnu.org>
2020-07-13 08:32:55 +0200

    dired: drop the skim conf as I'm not on a Mac

--- config/dired-conf.el
+++ config/dired-conf.el
@@ -16,16 +16,9 @@
       delete-by-moving-to-trash t
       ;; trash-directory "~/.Trash/emacs"
       dired-dwim-target t
-      ;; I installed Skim via Homebrew: brew search skim
-      ;; and created a shell script as follows:
-      ;; #!/bin/sh
-      ;; osascript -e "tell application \"Skim\"" -e "open \"$(pwd)/$1\"" -e "end tell"
       dired-guess-shell-alist-user
-      (if (file-exists-p "/usr/bin/evince")
-	  '(("\\.pdf\\'" "evince")
+      	  '(("\\.pdf\\'" "evince")
 	    ("\\.jpg\\'" "feh"))
-	'(("\\.pdf\\'" "skim")
-	  ("." "open")))
       dired-listing-switches "-alv")
 
 (add-hook 'image-dired-thumbnail-mode-hook
