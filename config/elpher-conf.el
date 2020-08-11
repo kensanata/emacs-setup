5163a47b2a4cf71d187b7607f6327c2f00e9c6f4
Alex Schroeder <alex@gnu.org>
2020-08-11 11:23:32 +0200

    elpher: various updates
    New keys for matching names on my wiki and for searching Wikipedia.
    Remove c-c Q binding.
    
    Fix how stuff gets loaded.

--- config/elpher-conf.el
+++ config/elpher-conf.el
@@ -21,11 +21,16 @@ Elements of ALIST that are not conses are ignored."
     alist))
 
 (global-set-key (kbd "C-c q") 'elpher-menu)
-(global-set-key (kbd "C-c Q") 'asc:elpher-match-alexschroeder.ch)
+
+(add-hook 'elpher-menu-mode-hook
+	  (lambda ()
+	    (local-set-key (kbd "n") (lambda () (interactive) (elpher 1)))
+	    (local-set-key (kbd "a") 'asc:elpher-match-alexschroeder.ch)
+	    (local-set-key (kbd "w") 'asc:elpher-search-en.wikipedia.org)))
 
 (defun asc:elpher-match-alexschroeder.ch (terms)
   (interactive "sTerms: ")
-  (let* ((name (format "*alexschroeder.ch match %s*" terms))
+  (let* ((name (format "*alexschroeder.ch*" terms))
 	 (buf (get-buffer-create name)))
     (pop-to-buffer-same-window buf)
     (elpher-mode)
@@ -36,15 +41,21 @@ Elements of ALIST that are not conses are ignored."
        (concat "gemini://alexschroeder.ch/do/match?"
 	       (url-hexify-string terms)))))))
 
+(defun asc:elpher-search-en.wikipedia.org (terms)
+  (interactive "sTerms: ")
+  (let* ((name (format "*en.wikipedia.org*" terms))
+	 (buf (get-buffer-create name)))
+    (pop-to-buffer-same-window buf)
+    (elpher-mode)
+    (elpher-visit-page
+     (elpher-make-page
+      (format "English Wikipedia: %s" terms)
+      (elpher-address-from-url
+       (concat "gemini://vault.transjovian.org/text/en/"
+	       (url-hexify-string terms)))))))
+
 ;; sometimes I'm in places with very bad connectivity
 (setq elpher-connection-timeout 20)
-
-(add-hook 'elpher-menu-mode-hook
-	  (lambda ()
-	    (local-set-key (kbd "e")
-			   (lambda ()
-			     (interactive)
-			     (elpher 1)))))
 
 (let ((version (emacs-version)))
   (when (and (string-match "^GNU Emacs \\([0-9]+\\)\.\\([0-9]+\\)" version)
@@ -56,16 +67,22 @@ Elements of ALIST that are not conses are ignored."
 
 (add-to-list 'load-path "/home/alex/src/elpher")
 (autoload 'elpher "elpher" "Gopher and Gemini client" t)
+(autoload 'elpher-menu "elpher" "An overview of all Elpher activities" t)
 
 (add-to-list 'load-path "/home/alex/src/gemini-write")
 (autoload 'elpher-edit "gemini-write" "Edit a Gemini page" t)
-(eval-after-load "elpher"
-  '(define-key elpher-mode-map (kbd "e") 'elpher-edit))
+;; cannot delay loading until the user uses e to edit a page because
+;; we need the advice for elpher-render-gemini-plain-text
+(eval-after-load "elpher" '(load-library "gemini-write"))
 
 (add-to-list 'load-path "/home/alex/src/gemini.el")
 (autoload 'gemini-mode "gemini-mode" "Gemini Mode" t)
 
 (add-hook 'gemini-mode-hook 'typo-mode)
+(add-hook 'gemini-mode-hook
+	  (lambda ()
+	    (local-set-key (kbd "C-c i") 'gemini-insert-link-to-oddmuse-wiki)
+	    (local-set-key (kbd "C-c l") 'gemini-insert-local-link)))
 
 (add-hook 'elpher-mode-hook
 	  (lambda ()
@@ -73,6 +90,19 @@ Elements of ALIST that are not conses are ignored."
 	    (local-set-key (kbd "b") 'elpher-back)
 	    (local-set-key (kbd "l") 'elpher-back)))
 
+(defun gemini-insert-local-link (pagename)
+  "Insert a link to a new page on this wiki."
+  (interactive "sPage name: ")
+  (let ((escaped (url-hexify-string pagename)))
+    (if (string= pagename escaped)
+	(insert "=> " pagename)
+      (insert "=> " escaped " " pagename))))
+
+(defun gemini-insert-link-to-oddmuse-wiki (pagename)
+  "Insert a link to PAGENAME of the current wiki with completion."
+  (interactive (list (oddmuse-read-pagename oddmuse-wiki)))
+  (insert "=> " pagename " " (replace-regexp-in-string "_" " " pagename)))
+  
 (defun elpher-node-up (node)
   "Does the node have a selector that looks like a path?"
   (let* ((address (elpher-node-address node))
