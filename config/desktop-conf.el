d65e23c3d982b5a3f5bbb5d88deddaf55f77e6ef
Alex Schroeder <alex@gnu.org>
2020-10-23 15:41:54 +0200

    Don't save encrypted files with the desktop
    If we do, then starting up Emacs asks us for the passphrase, which is annoying.

--- config/desktop-conf.el
+++ config/desktop-conf.el
@@ -1,5 +1,8 @@
 (setq desktop-restore-frames nil
-      desktop-load-locked-desktop t)
+      desktop-load-locked-desktop t
+      ;; don't save remote files, don't save encrypted files
+      desktop-files-not-to-save "\\(\\`/[^/:]*:\\|(ftp)\\'\\|\\.gpg\\'\\)"
+      desktop-modes-not-to-save '(tags-table-mode dired-mode))
 (desktop-save-mode 1)
 
 ;; save minibuffer history
