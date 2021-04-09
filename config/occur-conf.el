263cfe9c0afd86cbb3c443bdff51087e95c862ce
Alex Schroeder <alex@gnu.org>
2021-04-09 22:14:39 +0200

    occur: remove moccur and noccur

--- config/occur-conf.el
+++ config/occur-conf.el
@@ -1,9 +1,9 @@
 ;; moccur checks all the buffers
-(use-package color-moccur)
+;; (use-package color-moccur)
 
 ;; noccur-dired: Perform ‘multi-occur’ with REGEXP in all dired marked files.
 ;; noccur-project: Perform ‘multi-occur’ with REGEXP in the current project files.
-(use-package noccur)
+;; (use-package noccur)
 
 ;; use + and - in occur mode
 (use-package occur-context-resize)
