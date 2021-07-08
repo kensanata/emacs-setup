b9504e95c261fbf7ed5111722d086f86ea0f97e7
Alex Schroeder <alex@gnu.org>
2021-07-08 15:16:45 +0200

    Get rid of C-c w and C-c W, never used it anyway
    M-x eww it is! And since Elpher now sets variables to handle gemini
    and gopher links, I no longer have to do it myself.
    
    Also droping the definition of shr-tag-label; does it even have an
    effect these days?

--- config/eww-conf.el
+++ config/eww-conf.el
@@ -1,7 +1,3 @@
-;; I want to use eww.
-(global-set-key (kbd "C-c w") 'eww)
-(global-set-key (kbd "C-c W") 'eww-list-bookmarks)
-
 ;; no need for cookies within Emacs
 ;; use M-x url-cookie-list to check
 (setq url-cookie-trusted-urls '()
@@ -10,16 +6,4 @@
       url-privacy-level 'paranoid
       browse-url-browser-function 'eww-browse-url)
 
-(advice-add 'eww-browse-url :around 'asc:eww-browse-url)
-
-(defun asc:eww-browse-url (original url &optional new-window)
-  "Handle gemini links."
-  (cond ((string-match-p "\\`\\(gemini\\|gopher\\)://" url)
-	 (require 'elpher)
-	 (elpher-go url))
-	(t (funcall original url new-window))))
-
-;; labels don't come in paragraphs
-(eval-after-load "shr"
-  '(defun shr-tag-label (dom)
-     (shr-generic dom)))
+;; make Eww more like Elpher
