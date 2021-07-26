bc0020f55027d82705ea2b634ab7c28b87c306a6
Alex Schroeder <alex@gnu.org>
2021-07-26 15:33:14 +0200

    bookmark: use m for search and M for mark

--- config/bookmark-conf.el
+++ config/bookmark-conf.el
@@ -3,4 +3,5 @@
 	    (local-set-key (kbd "TAB") #'next-line)
 	    (local-set-key (kbd "<backtab>") #'previous-line)
 	    (local-set-key "j" #'bookmark-jump)
-	    (local-set-key "m" #'bookmark-bmenu-search)))
+	    (local-set-key "m" #'bookmark-bmenu-search)
+	    (local-set-key "M" #'bookmark-bmenu-mark)))
