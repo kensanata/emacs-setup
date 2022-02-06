e8eaf48486801c0ffcfaa368cb5f7d14908e2f4e
Alex Schroeder <alex@gnu.org>
2022-02-06 13:07:57 +0100

    Internet time, display time

--- config/calendar-conf.el
+++ config/calendar-conf.el
@@ -5,3 +5,42 @@
 				 "Juni" "Juli" "August" "September"
 				 "Oktober" "November" "Dezember"])
 
+(setq display-time-world-list
+      '(("America/New_York" "New York")
+	("Europe/Lisbon" "Lissabon")
+	("Europe/Zurich" "Zürich")))
+
+(defun itime-correct ()
+  "When point is on or after a local time, translate it into Internet Time."
+  (interactive)
+  (skip-chars-backward "0-9: \t")
+  (skip-chars-forward " \t")
+  (when (looking-at "\\([0-9]+\\):\\([0-9]+\\):?\\([0-9]+\\)?")
+    (let ((hours (match-string 1))
+	  (minutes (match-string 2))
+	  (seconds (match-string 3)))
+      (when (not seconds)
+	(setq seconds "0"))
+      (replace-match (itime-string hours minutes seconds)))))
+
+(defun itime-string (hour minute second &optional ignore)
+  "Return internet time as string.
+    HOUR MINUTE and SECOND are strings as provided within
+    `display-time-string-forms' and are local time."
+  (let* ((seconds (+ (* 3600 (string-to-number hour))
+		     3600
+		     (- (car (current-time-zone)))
+		     (* 60 (string-to-number minute))
+		     (string-to-number second)))
+	 (beats (mod (floor seconds 86.4) 1000)))
+    (format "@%03d" beats)))
+
+(setq display-time-string-forms
+     '(" " 24-hours ":" minutes " "
+       (itime-string 24-hours minutes seconds t)
+       (if mail
+	   " Mail"
+	 ""))
+     display-time-interval 5)
+
+(display-time-mode 1)
