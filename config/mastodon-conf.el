cb728c7f65744f5e12ec874a21fa0fa4b286f46a
Alex Schroeder <alex@gnu.org>
2022-05-15 11:40:22 +0200

    Mastodon timeline with avatars

--- config/mastodon-conf.el
+++ config/mastodon-conf.el
@@ -1,2 +1,5 @@
 (setq mastodon-instance-url "https://octodon.social"
-      mastodon-auth-source-file "~/.authinfo.gpg")
+      mastodon-auth-source-file "~/.authinfo.gpg"
+      mastodon-tl--show-avatars t
+      mastodon-media--enable-image-caching t
+      mastodon-toot--enable-custom-instance-emoji t)
