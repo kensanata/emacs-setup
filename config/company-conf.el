55641bf55e14ff7bfda47d4ecf850a7ec3cefe0f
Alex Schroeder <alex@gnu.org>
2022-05-15 11:43:44 +0200

    Get rid of company mode.

--- config/company-conf.el
+++
@@ -1,6 +0,0 @@
-(use-package company)
-(global-company-mode 1)
-
-;; support CamelCase as illustrated here:
-;; https://github.com/company-mode/company-mode/issues/60
-(setq company-dabbrev-downcase nil)
