451d861c2eacc91193e715f80de85859d6f24d90
Alex Schroeder <alex@gnu.org>
2022-06-09 22:59:09 +0200

    Add asc:move-mentioned-files

--- config/files-conf.el
+++ config/files-conf.el
@@ -1,2 +1,20 @@
 ;; Load up to 100MB without asking for confirmation (default is 10MB)
 (setq large-file-warning-threshold 100000000)
+
+(defun asc:move-mentioned-files (regexp target)
+  "Move all the filenames matching REGEXP to TARGET.
+The REGEXP matches any string in the buffer.
+If TARGET does not exist, it is created.
+This is useful for the output of where-was-it."
+  (interactive (list (read-string "Filenames matching: " "IMG_[0-9]+\\.JPG")
+		     (read-directory-name "Move to directory: ")))
+  (unless (file-directory-p target)
+    (make-directory target t))
+  (save-excursion
+    (goto-char (if (region-active-p) (region-beginning) (point-min)))
+    (let ((n 0))
+      (while (re-search-forward regexp (if (region-active-p) (region-end)) t)
+	(rename-file (match-string 0) (expand-file-name (match-string 0) target))
+	(setq n (1+ n)))
+      (message "Moved %d files." n))))
+
