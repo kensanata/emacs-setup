4530e511e24f1129f04af69b78744214cfaf1f8b
Alex Schroeder <alex@gnu.org>
2022-07-29 00:22:58 +0200

    Use cl-delete-if instead of delete-if
    I hate this…

--- config/picture-conf.el
+++ config/picture-conf.el
@@ -1,6 +1,6 @@
 (require 'cl-lib)
 ;; getting rid of image-mode for svg files is hard
 (setq auto-mode-alist
-      (delete-if (lambda (item)
+      (cl-delete-if (lambda (item)
 		   (eq (cdr item) 'image-mode))
 		 auto-mode-alist))
