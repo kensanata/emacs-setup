3fe1db0eb61c69c60bb5e67d922e46032ae77166
Alex Schroeder <alex@gnu.org>
2023-02-22 07:07:39 +0100

    use-package for color-moccur

--- config/occur-conf.el
+++ config/occur-conf.el
@@ -1,9 +1,11 @@
-;; moccur checks all the buffers
-;; (use-package color-moccur)
-
 ;; noccur-dired: Perform ‘multi-occur’ with REGEXP in all dired marked files.
 ;; noccur-project: Perform ‘multi-occur’ with REGEXP in the current project files.
 ;; (use-package noccur)
 
 ;; use + and - in occur mode
-(use-package occur-context-resize)
+(use-package occur-context-resize
+  :ensure t)
+
+(use-package color-moccur
+  :ensure t
+  :bind ("C-c O" . multi-occur-in-matching-buffers))
