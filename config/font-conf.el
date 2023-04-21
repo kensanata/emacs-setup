415d1b96ebe4587b172a2bf39ad809e1a9281f15
Alex Schroeder <alex@gnu.org>
2023-04-21 23:22:40 +0200

    Use Iosevka Term and Iosevka Etoile
    Get rid of Noto, never used it anyway.

--- config/font-conf.el
+++ config/font-conf.el
@@ -1,19 +1,17 @@
-(cond ((find-font (font-spec :name "Iosevka"))
-       ;; on windows, this defaults to a different font?
-       (dolist (face '(default fixed-pitch))
-	 (set-face-attribute
-	  face nil
-	  :family "Iosevka"
-	  :height (if (eq (window-system) 'w32) 180 220))))
-      ((find-font (font-spec :name "Noto Mono"))
-       (set-face-attribute 'default nil :family "Noto Mono" :height 200)))
+(when (find-font (font-spec :name "Iosevka Term"))
+  (dolist (face '(default fixed-pitch))
+    (set-face-attribute
+     face nil
+     :family "Iosevka Term"
+     :height (if (eq (window-system) 'w32) 180 220)))
+  (dolist (face '(variable-pitch))
+    (set-face-attribute
+     face nil
+     :family "Iosevka Etoile"
+     :height (if (eq (window-system) 'w32) 180 220))))
 
 (defun iosevka ()
   "Switch to Iosevka font."
   (interactive)
-  (set-face-attribute 'default nil :family "Iosevka"))
-
-(defun noto ()
-  "Switch to Noto font."
-  (interactive)
-  (set-face-attribute 'default nil :family "Noto Mono"))
+  (set-face-attribute 'default nil :family "Iosevka Term")
+  (set-face-attribute 'default nil :family "Iosevka Etoile"))
