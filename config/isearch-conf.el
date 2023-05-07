af5ae9e3b431ef05dc3af52b3e4f46441c0c3194
Alex Schroeder <alex@gnu.org>
2023-05-07 23:23:42 +0200

    Use swiper

--- config/isearch-conf.el
+++ config/isearch-conf.el
@@ -1,3 +1,9 @@
 ;; isearch occur no more: using swiper instead
 ;; (define-key isearch-mode-map (kbd "C-o") 'isearch-occur)
+
+(use-package swiper :ensure t
+  :bind ("C-c o" . swiper-thing-at-point)
+  :bind ("C-o" . swiper)
+  :bind ("C-M-o" . swiper-all))
+
 (define-key isearch-mode-map (kbd "C-o") 'swiper-from-isearch)
