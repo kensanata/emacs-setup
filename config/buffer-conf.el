c29c9dddec4be9dc277893ea6458430539518a0b
Alex Schroeder <alex@gnu.org>
2023-07-24 12:43:29 +0200

    Set ibuffer-formats

--- config/buffer-conf.el
+++ config/buffer-conf.el
@@ -1,5 +1,15 @@
 ;;; ibuffer
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 
+;; Possible elements: mark modified read-only locked name size mode
+;; process filename
+(setq ibuffer-formats
+      '((mark modified read-only locked
+              " " (name 40 40 :left :elide)
+              " " filename-and-process)
+        (mark " " (name 16 -1)
+              " " filename)))
+
+
 ;; midnight
 (midnight-mode 1)
