4b79aad4742f5ed3acb63bcc04b6f25c4da29bb7
Alex Schroeder <alex@gnu.org>
2023-09-27 09:45:50 +0200

    Switch to hunspell.

--- config/spell-conf.el
+++ config/spell-conf.el
@@ -1,18 +1,9 @@
+(setq ispell-program-name "hunspell")
+
 (defun swiss ()
   "Switch ispell dictionary to 'swiss'."
   (interactive)
-  (ispell-change-dictionary "swiss8")
-  (asc:flyspell)
-  (typo-mode 1)
-  (setq typo-language "German")
-  (set-input-method 'german-prefix))
-
-(defalias 'german 'swiss)
-
-(defun deutsch ()
-  "Switch ispell dictionary to 'swiss'."
-  (interactive)
-  (ispell-change-dictionary "german-new8")
+  (ispell-change-dictionary "de_CH")
   (asc:flyspell)
   (typo-mode 1)
   (setq typo-language "German")
@@ -21,28 +12,10 @@
 (defun english ()
   "Switch ispell dictionary to 'british'."
   (interactive)
-  (ispell-change-dictionary "british")
+  (ispell-change-dictionary "en_GB")
   (asc:flyspell)
   (typo-mode 1)
   (setq typo-language "English")
-  (set-input-method nil))
-
-(defun francais ()
-  "Switch ispell dictionary to 'francais'."
-  (interactive)
-  (ispell-change-dictionary "francais")
-  (asc:flyspell)
-  (typo-mode 1)
-  (setq typo-language "French")
-  (set-input-method 'french-prefix))
-
-(defun portuges ()
-  "Switch ispell dictionary to 'portuguese'."
-  (interactive)
-  (ispell-change-dictionary "portugues")
-  (asc:flyspell)
-  (typo-mode 1)
-  (setq typo-language "French")
   (set-input-method nil))
 
 (defun asc:flyspell ()
@@ -51,32 +24,3 @@
   (unless flyspell-mode
     (flyspell-mode t))
   (flyspell-buffer))
-
-(eval-after-load "ispell"
-  '(defun ispell-get-decoded-string (n)
-     "Get the decoded string in slot N of the descriptor of the current dict."
-     (let* ((slot (or
-		   (assoc ispell-current-dictionary ispell-local-dictionary-alist)
-		   (assoc ispell-current-dictionary ispell-dictionary-alist)
-		   (error "No data for dictionary \"%s\" in `ispell-local-dictionary-alist' or `ispell-dictionary-alist'"
-			  ispell-current-dictionary)))
-            (str (nth n slot)))
-       (when (stringp str)
-	 ;; no decoding!
-         str))))
-
-;; Fix encoding? Check with words such as "Tätigkeitsliste"
-;; ispell-dictionary-alist has latin-1 encoded umlauts!
-;; (assoc "swiss8" ispell-dictionary-alist)
-
-;; Also, the British dictionary doesn't recognise type mode stuff such
-;; as "doesn’t".
-;; (assoc "british" ispell-dictionary-alist)
-
-(setq ispell-local-dictionary-alist
-      '(("swiss8" "[A-Za-zÄÜÖäüöß]" "[^A-Za-zÄÜÖäüöß]"
-	 "[']" t ("-C" "-d" "de_CH") nil utf-8)
-	("francais" "[A-Za-zÀÂÆÇÈÉÊËÎÏÔÙÛÜàâçèéêëîïôùûü]" "[^A-Za-zÀÂÆÇÈÉÊËÎÏÔÙÛÜàâçèéêëîïôùûü]"
-	 "[-'’.@]" t ("-C" "-d" "fr_CH") nil utf-8)
-	("british" "[[:alpha:]]" "[^[:alpha:]]"
-	 "['’]" t ("-d" "en_GB") nil utf-8)))
