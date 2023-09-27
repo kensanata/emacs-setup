4c08dd3815947aa769f9c4675ab2239716515808
Alex Schroeder <alex@gnu.org>
2023-09-27 09:43:14 +0200

    Use desktop for variables but not for files

--- config/desktop-conf.el
+++ config/desktop-conf.el
@@ -1,8 +1,9 @@
 (setq desktop-restore-frames nil
       desktop-load-locked-desktop t
       ;; don't save remote files, don't save encrypted files
-      desktop-files-not-to-save "\\(\\`/[^/:]*:\\|(ftp)\\'\\|\\.gpg\\'\\)"
-      desktop-modes-not-to-save '(tags-table-mode dired-mode rmail-mode eww-mode))
+      ;; desktop-files-not-to-save "\\(\\`/[^/:]*:\\|(ftp)\\'\\|\\.gpg\\'\\)"
+      ;; desktop-modes-not-to-save '(tags-table-mode dired-mode rmail-mode eww-mode
+      desktop-files-not-to-save "")
 (desktop-save-mode 1)
 
 ;; save minibuffer history
