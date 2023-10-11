bd58c833350f5e863882da7fbd5bf9c52f9518b0
Alex Schroeder <alex@gnu.org>
2023-10-11 11:04:20 +0200

    Don't save Info-mode

--- config/desktop-conf.el
+++ config/desktop-conf.el
@@ -2,7 +2,7 @@
       desktop-load-locked-desktop t
       ;; don't save remote files, don't save encrypted files
       ;; desktop-files-not-to-save "\\(\\`/[^/:]*:\\|(ftp)\\'\\|\\.gpg\\'\\)"
-      ;; desktop-modes-not-to-save '(tags-table-mode dired-mode rmail-mode eww-mode
+      desktop-modes-not-to-save '(tags-table-mode dired-mode rmail-mode eww-mode Info-mode)
       desktop-files-not-to-save "")
 (desktop-save-mode 1)
 
