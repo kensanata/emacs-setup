09a811ce8616fa030fefac40574b83ab73c4f4d3
Alex Schroeder <alex@gnu.org>
2023-10-31 14:20:21 +0100

    edit-server config for editing from the browser

---
+++ config/edit-server-conf.el
@@ -0,0 +1,16 @@
+(use-package edit-server
+  :ensure t
+  :commands edit-server-start
+  :init (if after-init-time
+              (edit-server-start)
+            (add-hook 'after-init-hook
+                      #'(lambda() (edit-server-start))))
+  :config (setq edit-server-new-frame-alist
+                '((name . "Edit with Emacs FRAME")
+                  (top . 200)
+                  (left . 200)
+                  (width . 80)
+                  (height . 25)
+                  (minibuffer . t)
+                  (menu-bar-lines . t)
+                  (window-system . x))))
