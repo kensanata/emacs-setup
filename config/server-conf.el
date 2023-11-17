4c8d3cce486d8e8369f40ad2269f393de10953d6
Alex Schroeder <alex@gnu.org>
2023-11-17 22:34:22 +0100

    No longer start the server
    Apparently Debian does this for me somehow??

--- config/server-conf.el
+++
@@ -1,6 +0,0 @@
-;; If we run with idle timer, then it doesn't matter if we run into an
-;; error. The remaining init code still runs.
-(run-with-idle-timer
- 10 nil
- (lambda ()
-   (server-start)))
