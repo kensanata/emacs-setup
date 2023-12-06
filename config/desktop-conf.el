39c7298f8dfa48cc080a53a2cacb2064086b06a5
Alex Schroeder <alex@gnu.org>
2023-12-06 21:47:22 +0100

    Save asc:file-names-seen with the desktop

--- config/desktop-conf.el
+++ config/desktop-conf.el
@@ -4,7 +4,10 @@
       ;; desktop-files-not-to-save "\\(\\`/[^/:]*:\\|(ftp)\\'\\|\\.gpg\\'\\)"
       desktop-modes-not-to-save '(tags-table-mode dired-mode rmail-mode eww-mode Info-mode)
       desktop-files-not-to-save "")
+
 (desktop-save-mode 1)
+
+(add-to-list 'desktop-globals-to-save 'asc:file-names-seen)
 
 ;; save minibuffer history
 (savehist-mode 1)
