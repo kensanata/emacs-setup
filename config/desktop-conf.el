38cc8fe1069ea8fbd7d2eb4a091b65eb771e6524
Alex Schroeder <alex@gnu.org>
2024-03-02 08:21:58 +0100

    Change comments

--- config/desktop-conf.el
+++ config/desktop-conf.el
@@ -1,8 +1,8 @@
 (setq desktop-restore-frames nil
       desktop-load-locked-desktop t
-      ;; don't save remote files, don't save encrypted files
-      ;; desktop-files-not-to-save "\\(\\`/[^/:]*:\\|(ftp)\\'\\|\\.gpg\\'\\)"
+      ;; don't save any buffers
       desktop-modes-not-to-save '(tags-table-mode dired-mode rmail-mode eww-mode Info-mode)
+      ;; don't save any files
       desktop-files-not-to-save "")
 
 (desktop-save-mode 1)
