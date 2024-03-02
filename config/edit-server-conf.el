43f2f42891d951ce52b19803996088cbe2e69fc2
Alex Schroeder <alex@gnu.org>
2024-03-02 08:22:25 +0100

    Don't set edit-server-new-frame-alist

--- config/edit-server-conf.el
+++ config/edit-server-conf.el
@@ -4,13 +4,14 @@
   :init (if after-init-time
               (edit-server-start)
             (add-hook 'after-init-hook
-                      #'(lambda() (edit-server-start))))
-  :config (setq edit-server-new-frame-alist
-                '((name . "Edit with Emacs FRAME")
-                  (top . 200)
-                  (left . 200)
-                  (width . 80)
-                  (height . 25)
-                  (minibuffer . t)
-                  (menu-bar-lines . t)
-                  (window-system . x))))
+                      #'(lambda() (edit-server-start)))))
+
+  ;; :config (setq edit-server-new-frame-alist
+  ;;               '((name . "Edit with Emacs FRAME")
+  ;;                 (top . 200)
+  ;;                 (left . 200)
+  ;;                 (width . 80)
+  ;;                 (height . 25)
+  ;;                 (minibuffer . t)
+  ;;                 (menu-bar-lines . t)
+  ;;                 (window-system . x)))
