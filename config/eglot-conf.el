5b5acee8816630d837c3db4786bf3ce2ed6a70d1
Alex Schroeder <alex@gnu.org>
2024-07-23 17:10:19 +0200

    Add eglot config

---
+++ config/eglot-conf.el
@@ -0,0 +1,18 @@
+;; Note that LSP integration only works in a "project": a VCS
+;; repository, or in the case of Marksman, a directory containing an
+;; (empty) .marksman.toml file.
+
+(add-hook 'go-mode-hook 'eglot-ensure)
+(add-hook 'perl-mode-hook 'eglot-ensure)
+(add-hook 'markdown-mode-hook 'eglot-ensure)
+(autoload 'eglot-ensure "eglot" "Start Eglot session for current buffer if there isn’t one.")
+
+(use-package eglot :ensure t
+  :config (add-to-list 'eglot-server-programs '(markdown-mode . ("marksman"))))
+
+;; (custom-set-variables '(eglot-ignored-server-capabilites '(list :documentHighlightProvider :hoverProvider :signatureHelpProvider)))
+
+(setq-default eglot-workspace-configuration
+    '((:gopls .
+        ((staticcheck . t)
+         (matcher . "CaseSensitive")))))
