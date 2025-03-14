9e680b16a3eab52f08c0969dc74b271d0fdad07b
Alex Schroeder <alex@gnu.org>
2025-03-14 20:43:24 +0100

    Apply ANSI mode to compilation buffers

--- config/compilation-conf.el
+++ config/compilation-conf.el
@@ -2,3 +2,10 @@
 
 (defun asc:compilation-mode-init ()
   (local-set-key (kbd "C-z") #'bury-buffer))
+
+(add-hook 'compilation-filter-hook 'ansi-mode-for-compilation)
+
+(defun ansi-mode-for-compilation ()
+  (let ((begin compilation-filter-start)
+        (end (point)))
+    (ansi-color-apply-on-region begin end)))
