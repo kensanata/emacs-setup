81baf17bd677a81e786f1418bb9e49c92ed8edbc
Alex Schroeder <alex@gnu.org>
2025-08-13 14:58:29 +0200

    Get rid of Windows stuff for Tramp

--- config/tramp-conf.el
+++ config/tramp-conf.el
@@ -3,24 +3,4 @@
       remote-file-name-inhibit-auto-save-visited t
       tramp-allow-unsafe-temporary-files t) ;; See (tramp) Auto-save File Lock and Backup
 
-(defvar putty-directory "c:/Portable Programs/PuTTY"
-  "The directory containing pscp.exe and plink.exe on Windows.")
-
 (setq tramp-default-method "ssh")
-(when (eq window-system 'w32)
-  (setq tramp-default-method "plink")
-  (when (and (not (string-match putty-directory (getenv "PATH")))
-	     (file-directory-p putty-directory))
-    (setenv "PATH" (concat putty-directory ";" (getenv "PATH")))))
-
-(defun sibirocobombus ()
-  "Connect to my favorite server using Tramp.
-On Windows, make sure PAGEANT is running and the sibirocobombus
-key has been added to it."
-  (interactive)
-  (find-file "/ssh:sibirocobombus:"))
-
-(defun the-shire ()
-  "Connect to a backup server using Tramp."
-  (interactive)
-  (find-file "/ssh:kensanata@theshire.emacs.cl:"))
