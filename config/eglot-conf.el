48abd0d950801debdb9d1ab6edf08e5a4ccec2e3
Alex Schroeder <alex@gnu.org>
2025-08-13 15:01:39 +0200

    Use eglot for (Chicken) scheme

--- config/eglot-conf.el
+++ config/eglot-conf.el
@@ -5,10 +5,13 @@
 (add-hook 'go-mode-hook 'eglot-ensure)
 (add-hook 'perl-mode-hook 'eglot-ensure)
 (add-hook 'markdown-mode-hook 'eglot-ensure)
+(add-hook 'scheme-mode-hook 'eglot-ensure)
 (autoload 'eglot-ensure "eglot" "Start Eglot session for current buffer if there isn’t one.")
 
 (use-package eglot :ensure t
-  :config (add-to-list 'eglot-server-programs '(markdown-mode . ("marksman"))))
+  :config (progn
+            (add-to-list 'eglot-server-programs '(markdown-mode . ("marksman")))
+            (add-to-list 'eglot-server-programs '(scheme-mode . ("chicken-lsp-server")))))
 
 ;; (custom-set-variables '(eglot-ignored-server-capabilites '(list :documentHighlightProvider :hoverProvider :signatureHelpProvider)))
 
