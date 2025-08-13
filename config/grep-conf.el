7e8ec3a9b862e1714319b466673b69db798a8237
Alex Schroeder <alex@gnu.org>
2025-08-13 15:01:24 +0200

    No null device for grep

--- config/grep-conf.el
+++ config/grep-conf.el
@@ -1,4 +1,7 @@
-(use-package "grep")
+(use-package "grep"
+  ;; the null device is a problem if I want to use git grep instead of
+  ;; grep and grep doesn't need it because my grep has -H support
+  :config (grep-apply-setting 'grep-use-null-device nil))
 
 (defun grep-word-at-point (word)
   "Grep for thing at point."
