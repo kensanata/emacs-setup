ca9b191de6cef3b25ef3f4dd9050c3c3836ca622
Alex Schroeder <alex@gnu.org>
2025-10-02 23:30:31 +0200

    Shell mode indentation

--- config/shell-conf.el
+++ config/shell-conf.el
@@ -1,1 +1,4 @@
 (add-to-list 'auto-mode-alist '("\\.fish\\'" . shell-script-mode))
+(add-hook 'sh-mode-hook 'asc:sh-mode-init)
+(defun asc:sh-mode-init ()
+  (local-set-key "TAB" 'sh-basic-indent-line))
